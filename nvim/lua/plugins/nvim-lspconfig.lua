18527876f91230b4c1afa2a37eca9a92eae395b3
Bruno Mazzo <mazzo.bruno@gmail.com>
2024-12-27 20:44:51 +1000

    Add typescript custom lsp config

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -32,6 +32,97 @@ return {
           },
         })
       end,
+      ["vtsls"] = function ()
+        lspconfig["vtsls"].setup({
+filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                maxInlayHintLength = 30,
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+          keys = {
+            {
+              "gD",
+              function()
+                local params = vim.lsp.util.make_position_params()
+                vim.lsp.buf.execute_command({
+                  command = "typescript.goToSourceDefinition",
+                  arguments = { params.textDocument.uri, params.position },
+                  open = true,
+                })
+              end,
+              desc = "Goto Source Definition",
+            },
+            {
+              "gR",
+              function()
+                vim.lsp.buf.execute_command({
+                  command = "typescript.findAllFileReferences",
+                  arguments = { vim.uri_from_bufnr(0) },
+                  open = true,
+                })
+              end,
+              desc = "File References",
+            },
+            -- {
+            --   "<leader>co",
+            --   LazyVim.lsp.action["source.organizeImports"],
+            --   desc = "Organize Imports",
+            -- },
+            -- {
+            --   "<leader>cM",
+            --   LazyVim.lsp.action["source.addMissingImports.ts"],
+            --   desc = "Add missing imports",
+            -- },
+            -- {
+            --   "<leader>cu",
+            --   LazyVim.lsp.action["source.removeUnused.ts"],
+            --   desc = "Remove unused imports",
+            -- },
+            -- {
+            --   "<leader>cD",
+            --   LazyVim.lsp.action["source.fixAll.ts"],
+            --   desc = "Fix all diagnostics",
+            -- },
+            {
+              "<leader>cV",
+              function()
+                vim.lsp.buf.execute_command({ command = "typescript.selectTypeScriptVersion" })
+              end,
+              desc = "Select TS workspace version",
+            },
+          },
+        }) 
+      end
     })
 
     local keymap = vim.keymap
@@ -44,22 +135,22 @@ return {
 
         -- set keybinds
         opts.desc = "Show LSP references"
-        keymap.set("n", "gr", vim.lsp.buf.references, opts) -- show definition, references
+        keymap.set("n", "gr", "<cmd>FzfLua lsp_references jump_to_single_result=true ignore_current_line=true<cr>", opts) -- show definition, references
 
         opts.desc = "Go to declaration"
         keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
         opts.desc = "Show LSP definitions"
-        keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definitions
+        keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions jump_to_single_result=true ignore_current_line=true<cr>", opts) -- show lsp definitions
 
         opts.desc = "Show LSP implementations"
-        keymap.set("n", "gi", vim.lsp.buf.implementation, opts) -- show lsp implementations
+        keymap.set("n", "gi", "<cmd>FzfLua lsp_implementations jump_to_single_result=true ignore_current_line=true<cr>", opts) -- show lsp implementations
 
         opts.desc = "Show LSP type definitions"
-        keymap.set("n", "gy", vim.lsp.buf.type_definition, opts) -- show lsp type definitions
+        keymap.set("n", "gy", "<cmd>FzfLua lsp_typedefs jump_to_single_result=true ignore_current_line=true<cr>", opts) -- show lsp type definitions
 
         opts.desc = "See available code actions"
-        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+        keymap.set({ "n", "v" }, "<leader>ca", "<cmd>FzfLua lsp_code_actions<cr>", opts) -- see available code actions, in visual mode will apply to selection
 
         opts.desc = "Smart rename"
         keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
