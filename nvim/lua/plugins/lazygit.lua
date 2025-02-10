c5e489220884883a80f0d2cab53c7a7e8f3120ae
Bruno Mazzo <mazzo.bruno@gmail.com>
2025-02-10 12:50:39 +1000

    add git pull command to lazygit

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -1,20 +1,45 @@
 return {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
+  "kdheepak/lazygit.nvim",
+  lazy = true,
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { "<leader>gg", "<cmd>lazygit<cr>", desc = "lazygit" },
+    {
+      "<leader>gp",
+      function()
+        local Job = require("plenary.job")
+        ---@diagnostic disable: missing-fields
+        vim.notify("Pulling latest changes from remote", "info", { title = "Git pull" })
+
+        Job:new({
+          command = "git",
+          args = {
+            "pull",
+          },
+          cwd = vim.loop.cwd(),
+          on_exit = function(j, return_val)
+            if return_val ~= 0 then
+              vim.notify("Failed to pull")
+              return
+            else
+              vim.notify("Pulled latest changes from remote", "info", { title = "Git pull" })
+            end
+          end,
+        }):start()
+      end,
+      desc = "Git pull",
     },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-        { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+  },
 }
