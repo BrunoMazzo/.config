70655f6ffed88bb14b0d5ce0de0cd9870d833c93
Bruno Mazzo <mazzo.bruno@gmail.com>
2025-03-14 13:05:13 +1000

    add auto close to neotree and some nesting logic

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,13 +2,13 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   cmd = "Neotree",
   dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
   keys = {
-        {
+    {
       "<leader>fe",
       function()
         require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
@@ -54,6 +54,17 @@ return {
     })
   end,
   opts = {
+    event_handlers = {
+      {
+        event = "file_open_requested",
+        handler = function()
+          -- auto close
+          -- vim.cmd("Neotree close")
+          -- OR
+          require("neo-tree.command").execute({ action = "close" })
+        end,
+      },
+    },
     sources = { "filesystem", "buffers", "git_status" },
     open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
     filesystem = {
@@ -62,8 +73,10 @@ return {
       use_libuv_file_watcher = true,
     },
     window = {
+      -- position = "current",
+      auto_expand_width = true,
       mappings = {
-        ["l"] = "open",
+        ["l"] = "toggle_node",
         ["h"] = "close_node",
         ["<space>"] = "none",
         ["Y"] = {
@@ -95,6 +108,16 @@ return {
           unstaged = "󰄱",
           staged = "󰱒",
         },
+      },
+    },
+    nesting_rules = {
+      ["tsx"] = {
+        pattern = "(.*)%.tsx$",
+        files = { "%1*.test.*", "%1*.spec.*" },
+      },
+      ["ts"] = {
+        pattern = "(.*)%.ts$",
+        files = { "%1*.test.*", "%1*.spec.*" },
       },
     },
   },
