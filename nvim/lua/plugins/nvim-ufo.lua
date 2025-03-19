03a5fb1864a683b06de24b4da033348e3c8ad041
Bruno Mazzo <mazzo.bruno@gmail.com>
2025-03-19 16:10:46 +1000

    reset nvim ufo to defaults

--- nvim/lua/plugins/nvim-ufo.lua
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -2,11 +2,8 @@ return {
   "kevinhwang91/nvim-ufo",
   dependencies = {'kevinhwang91/promise-async'},
   config = function()
-    require("ufo").setup({
-      provider_selector = function(bufnr, filetype, buftype)
-        return { "treesitter", "indent" }
-      end,
-    })
+    require("ufo").setup()
+
     vim.keymap.set("n", "zR", require("ufo").openAllFolds)
     vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
   end,
